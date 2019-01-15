3ef5774222c4eb0188d71495ce3ac502db4e770d
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-01-15 11:58:22 -0800

    add background and start on scripts

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(function() {
+  $(".clickable").click(function() {
+  $("#chan-hidden").toggle();
+  $("#morel-hidden").toggle();
+  $("#fly-hidden").toggle();
+  $("#mia-hidden").toggle();
+  $("#enoki-hidden").toggle();
+  });
+});
