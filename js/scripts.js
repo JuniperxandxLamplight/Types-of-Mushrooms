4d5bd1c53f33a461bef9f10a5559175846f31c1a
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-01-15 12:54:19 -0800

    add content to elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
 
   $(".chantrelle .clickable").click(function() {
-    $("#chan-showing").toggle();
-    $("#chan-hidden").toggle();
+    $("#chan-showing").fadeToggle();
+    $("#chan-hidden").fadeToggle();
   });
 
   $(".morel .clickable").click(function() {
-    $("#morel-showing").toggle();
-    $("#morel-hidden").toggle();
+    $("#morel-showing").fadeToggle();
+    $("#morel-hidden").fadeToggle();
   });
 
   $(".fly .clickable").click(function() {
-    $("#fly-showing").toggle();
-    $("#fly-hidden").toggle();
+    $("#fly-showing").fadeToggle();
+    $("#fly-hidden").fadeToggle();
   });
 
-  $(".miataki .clickable").click(function() {
-    $("#mia-showing").toggle();
-    $("#mia-hidden").toggle();
+  $(".miatake .clickable").click(function() {
+    $("#mia-showing").fadeToggle();
+    $("#mia-hidden").fadeToggle();
   });
 
   $(".enoki .clickable").click(function() {
-    $("#enoki-showing").toggle();
-    $("#enoki-hidden").toggle();
+    $("#enoki-showing").fadeToggle();
+    $("#enoki-hidden").fadeToggle();
   });
 
 });
